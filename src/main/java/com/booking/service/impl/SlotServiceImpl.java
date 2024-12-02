0d544a6564b1470b460ad1c2796e3833e8b92a90
vijay <vijayp@helixotech.com>
2024-12-02 13:53:30 +0530

    Pushed Prod db

--- src/main/java/com/booking/service/impl/SlotServiceImpl.java
+++ src/main/java/com/booking/service/impl/SlotServiceImpl.java
@@ -467,14 +467,14 @@ public class SlotServiceImpl implements SlotService {
 		
 		 try {	
 			Class.forName("org.postgresql.Driver");
-//			conn = DriverManager
-//			            .getConnection("jdbc:postgresql://database-1.cnaee60qc6yl.us-east-2.rds.amazonaws.com/clubdb",
-//			            		userName, password);
+			conn = DriverManager
+			            .getConnection("jdbc:postgresql://database-1.cnaee60qc6yl.us-east-2.rds.amazonaws.com/clubdb",
+			            		userName, password);
 			
 			//////dev db
-			conn = DriverManager
-		            .getConnection("jdbc:postgresql://clandb.ctiytm0zhzt8.ap-south-1.rds.amazonaws.com/ClubDB",
-		            		userName, password);
+//			conn = DriverManager
+//		            .getConnection("jdbc:postgresql://clandb.ctiytm0zhzt8.ap-south-1.rds.amazonaws.com/ClubDB",
+//		            		userName, password);
 ////			  conn.setAutoCommit(false);
 				//String slotSaveSQl= "INSERT INTO public.club_slot( club_name) 	VALUES ( ?)";
 
